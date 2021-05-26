bb7a59ce7c9ccb2be20da9962bda27049e04f3ab
soyalextreme <soyalextreme@gmail.com>
2021-05-25 23:33:00 -0500

    Doctest modulo terminado

--- algoritmos.py
+++ algoritmos.py
@@ -1,3 +1,12 @@
+"""
+>>> recursivo = Recursivo()
+>>> recursivo.factorial(5)
+120
+>>> recursivo.factorial(13)
+6227020800
+"""
+
+
 def fibonacci(number):
     if number == 0: return 0
     elif number == 1: return 1
@@ -13,12 +22,24 @@ def palindromo(sentence):
     sentence -- string o entero
 
     >>> palindromo("anita lava la tina")
-    True 
-    >>> palindromo(12321)
     True
-    >>> palindromo("Mi mama me mima")
+    >>> palindromo(12321)
     True
     """
     sentence = str(sentence).lower().replace(" ", "")
     return sentence == sentence[::-1]
 
+
+class Recursivo:
+    def factorial(self, number):
+        if number == 0: 
+            return 1
+        else: 
+            return number * self.factorial(number - 1)
+
+
+if __name__ == "__main__":
+    # colaca la flag -v para ver todos los resultados
+    import doctest
+    doctest.testmod()
+    doctest.testfile("./doct_test.txt")
