eae5cb5b786516cc276203b701502d99cf2e65f5
soyalextreme <soyalextreme@gmail.com>
2021-05-25 23:11:25 -0500

    Probandolo en las doc strings

--- algoritmos.py
+++ algoritmos.py
@@ -3,3 +3,22 @@ def fibonacci(number):
     elif number == 1: return 1
     else: return fibonacci(number - 1) + fibonacci(number - 2)
 
+
+def palindromo(sentence):
+    """
+    Retorna verdadero si el parametro es un palindromo 
+    en caso contrario retorna falso.
+
+
+    sentence -- string o entero
+
+    >>> palindromo("anita lava la tina")
+    True 
+    >>> palindromo(12321)
+    True
+    >>> palindromo("Mi mama me mima")
+    True
+    """
+    sentence = str(sentence).lower().replace(" ", "")
+    return sentence == sentence[::-1]
+
